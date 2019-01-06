b6754b17bc5e0d6b9628f55dceb200a59a6350b0
fredrik.palmgren@gmail.com <fredrik.palmgren@gmail.com>
2019-01-06 14:33:09 +0100

    Loop for 10*5min added. Lamp changed to RED/BLUE/RED depending on disturbances Älvsjö-Kista

--- SLdistif.py
+++ SLdistif.py
@@ -2,6 +2,7 @@
 
 # Import some libs
 import sys
+import config # The file containing the SL API and other stuff
 import requests
 import json
 
@@ -15,19 +16,24 @@ def getMainPathDisturbances(travelingPath):
     numberOfMainDisturbances = len(MainDisturbances_dict)
     numberOfLowPrioDisturbances = len(LowPrioDisturbances_dict)
 
-    if numberOfMainDisturbances >= 1:
-        print("High prio disturbances exists ("+str(numberOfMainDisturbances)+")")
-        for disturbanceItem in MainDisturbances_dict:
-            printKeyDistInfo(disturbanceItem)
-    else:
-        print("No High prio disturbances on the traveling path")
+    printout = False # Do something else with this. Separate print function or use as debug thingy only
+    if printout == True:
+        if numberOfMainDisturbances >= 1:
+            print("High prio disturbances exists ("+str(numberOfMainDisturbances)+")")
+            for disturbanceItem in MainDisturbances_dict:
+                printKeyDistInfo(disturbanceItem)
+        else:
+            print("No High prio disturbances on the traveling path")
 
-    if numberOfLowPrioDisturbances >= 1:
-        print("Low prio disturbances exists ("+str(numberOfLowPrioDisturbances)+")")
-        for disturbanceItem in LowPrioDisturbances_dict:
-            printKeyDistInfo(disturbanceItem)
-    else:
-        print("No Low prio disturbances on the traveling path")
+        if numberOfLowPrioDisturbances >= 1:
+            print("Low prio disturbances exists ("+str(numberOfLowPrioDisturbances)+")")
+            for disturbanceItem in LowPrioDisturbances_dict:
+                printKeyDistInfo(disturbanceItem)
+        else:
+            print("No Low prio disturbances on the traveling path")
+
+    # Return only the MainNews disturbances
+    return MainDisturbances_dict
 
 
 def getPathDisturbances(travelingPath):
@@ -52,7 +58,7 @@ def getDisturbances(transportMode, lineNumber):
     r = requests.get(req)
     print(r.status_code)
     if(r.status_code == 200):
-        print ("All OK")
+        #print ("All OK")
         issues = json.loads(r.text)["ResponseData"]
         return issues
     else:
@@ -62,7 +68,7 @@ def getDisturbances(transportMode, lineNumber):
 
 def createRequest(transportMode, lineNumber):
     # Create the request to send towards the SL disturbance service end-point
-    SLdistAPIkey = '430c2aeb3fea4287bc2c882b9752a1e0'
+    SLdistAPIkey = config.SLdistAPIkey # API-key to be added in config.py with one line "SLdistAPIkey = <YOUR-API-KEY>""
     req = 'http://api.sl.se/api2/deviations.Json?key='+SLdistAPIkey+'&transportMode='+transportMode+'&lineNumber='+lineNumber
     return req
 
@@ -118,23 +124,30 @@ def printKeyDistInfo(disturbanceItem):
 # This instead of using positional arguments
 # Perhaps we should move to this one instead?
 # It become a bit more messy to call when building the function calls, but more future proof?
-def createRequest2(**options):
-    APIkey = '430c2aeb3fea4287bc2c882b9752a1e0'
-    
-    # If any of the optional arguments were given, apply them
-    if (options.get("transportMode") == "train" or options.get("transportMode") == "metro"):
-        print("you only care about the "+options.get("transportMode"))
-        transportMode = options.get("transportMode")
-    else:
-        print("all transports it is as you did not select any valid transportMode")
-        transportMode = ""
-
-    if options.get("lineNumber"):
-        print("you only care about the lines "+options.get("lineNumber"))
-        lineNumber = options.get("lineNumber")
-    elif not options.get("lineNumber"):
-        print("all lineNumbers it is as you did not select any valid lineNumber")
-        lineNumber = ""
-        
-    req = 'http://api.sl.se/api2/deviations.Json?key='+APIkey+'&transportMode='+transportMode+'&lineNumber='+lineNumber
-    return req
+#def createRequest2(**options):
+#    APIkey = config.SLdistAPIkey
+#    
+#    # If any of the optional arguments were given, apply them
+#    if (options.get("transportMode") == "train" or options.get("transportMode") == "metro"):
+#        print("you only care about the "+options.get("transportMode"))
+#        transportMode = options.get("transportMode")
+#    else:
+#        print("all transports it is as you did not select any valid transportMode")
+#        transportMode = ""
+#
+#    if options.get("lineNumber"):
+#        print("you only care about the lines "+options.get("lineNumber"))
+#        lineNumber = options.get("lineNumber")
+#    elif not options.get("lineNumber"):
+#        print("all lineNumbers it is as you did not select any valid lineNumber")
+#        lineNumber = ""
+#        
+#    req = 'http://api.sl.se/api2/deviations.Json?key='+APIkey+'&transportMode='+transportMode+'&lineNumber='+lineNumber
+#    return req
+#
+#
+# Bonus info: If playing with importing modules to jupyter notebook, you can make sure your updates in the libs (.py) are reoaded by adding
+# %load_ext autoreload
+# %autoreload 2
+# %reload_ext autoreload
+# import SLdistif as sl
--- reactOnSL.py
+++ reactOnSL.py
@@ -1,12 +1,13 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # This is the main program
 # Intended to run in a loop during the time there is an interest of soon traveling
 # e.g. in the morning before going to work
 # If there are any high priority disturbances on the travel path (e.g. to work), 
-# a lamp will turn RED. If there is not but there are unusually many low prio issues, lamp will turn yellow
+# a lamp will turn RED. If there is not but there are unusually many low prio issues, lamp will turn BLUE
 # If all are OK (no high prio and not that many low prio), then turn lamp GREEN 
-# To decide how this is to be run and where scheduling shall be handled. E.g. no use to mess with lamp if no-one home etc.
-
+#
+# To decide how this is to be triggered and where scheduling shall be handled. E.g. no use to mess with lamp if no-one home etc.
+# One way could be to wait for Philips Hue motion sensor going off in the morning before starting up
 
 from phue import Bridge
 from datetime import datetime
@@ -25,33 +26,56 @@ def setupHue():
 def main():
 	# Get bridge state and full dictonary
 	b = setupHue()
+	kickOffLoop(b)
+
+	# Get a dictionary with the light name as the key
+	#light_names = b.get_light_objects('name')
+	#print(light_names)
+
+def kickOffLoop(b):
 	# Get a dictionary with the light name as the key
 	light_names = b.get_light_objects('name')
 
+	# Loop for 50 minutes (10 times, check every five minutes)
 	for x in range(10):
+		# Get the current disturbances on the specified travelling path
 		dist = sl.getPathDisturbances("Älvsjö-Kista")
+		# Print the disturbances out
+		print("--------------")
+		print(datetime.now().time())
 		sl.printDistHeaders(dist)
+		# Split out the high vs low prio disturbances and count them
 		MainDisturbances_dict = [x for x in dist if x['MainNews']==True]
 		LowPrioDisturbances_dict = [x for x in dist if x['MainNews']==False]
 		numberOfMainDisturbances = len(MainDisturbances_dict)
 		numberOfLowPrioDisturbances = len(LowPrioDisturbances_dict)
 		
-		print(datetime.now().time())
-		if (numberOfLowPrioDisturbances > 4):
-			print("Many LowPrio disturbances, ("+str(numberOfLowPrioDisturbances)+"), better check")
+
+		# IF there are at least one high prio disturbance, turn RED
+		if (numberOfMainDisturbances >=1):
+			print("High prio disturbances ("+str(numberOfMainDisturbances)+"), please check!")
 			light_names["Sektetär"].on = True
 			light_names["Sektetär"].brightness = 254
-			light_names["Sektetär"].hue = 15000
-		elif (numberOfLowPrioDisturbances <= 4):
-			print("Just a few LowPrio disturbances ("+str(numberOfLowPrioDisturbances)+"), just relax")
+			light_names["Sektetär"].hue = 65535 # RED
+			# Here also add some more notification e.g. play sound on Sonos and/or
+			# push info on the disturbance to the mobile / google home screen etc.
+
+		# else if there are quite some low prio disturbances, turn BLUE
+		elif (numberOfLowPrioDisturbances > 10):
+			print("Many LowPrio disturbances ("+str(numberOfLowPrioDisturbances)+"), better check")
+			light_names["Sektetär"].on = True
+			light_names["Sektetär"].brightness = 254
+			light_names["Sektetär"].hue = 46920 #BLUE
+
+		# and if no high prio and only low number of low prio, tyrn GREEN
+		elif (numberOfLowPrioDisturbances <= 10):
+			print("Just a few LowPrio disturbances ("+str(numberOfLowPrioDisturbances)+"), you can relax")
 			light_names["Sektetär"].on = True
 			light_names["Sektetär"].brightness = 150
-			light_names["Sektetär"].hue = 5000
-
-		time.sleep(60)
+			light_names["Sektetär"].hue = 25500 # GREEN
+		print("+++++++++++++++")
+		# Sleep for 5 minutes
+		time.sleep(5*60)
   
 if __name__== "__main__":
   main()
-
-
-
